2480783264ee143919b9f5e4db941b743c725156
Edouard <ek4149@princeton.edu>
2024-01-15 14:14:47 -0500

    added navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Container } from 'react-bootstrap';
+import Header from './components/header';
 
 const App = () => {
   return (
-    <h2>
-      Welcome to ProShop
-    </h2>
+    <>
+        <Header />
+        <main className='py-3'>
+
+          <Container>
+            <h2> Welcome to ProShop</h2>
+          </Container>
+
+        </main>
+    </>
   )
 }
 
